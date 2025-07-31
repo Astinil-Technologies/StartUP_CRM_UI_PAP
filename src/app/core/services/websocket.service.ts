c035b5caa04640e6b09645bab6e5275b77b06b01
ranjit sahoo <mailtoranjit96@gmail.com>
2025-07-31 17:19:00 +0530

    Developed STRT-MEET-FE-007: Video Meeting feature with screen share and chat layout

--- src/app/core/services/websocket.service.ts
+++ src/app/core/services/websocket.service.ts
@@ -22,7 +22,6 @@ export class WebSocketService {
 
     const socket = new WebSocket('ws://localhost:8080/ws');
     this.stompClient = Stomp.over(socket);
-    this.stompClient.debug = () => {}; // Disable logs
     this.currentRoomId = roomId;
 
     this.stompClient.connect({}, () => {
