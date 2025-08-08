4cf29d9e901d699f3a0262150625ffb3c7a80d30
Sourabhpatil <162549311+SourabhGpatilsp@users.noreply.github.com>
2025-08-09 01:55:08 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 const BASE_URL = 'http://localhost:8080';
 
 export const environment = {
-    production: true,
+    production: false,
     baseUrl:`${BASE_URL}`,
     apiUrl: `${BASE_URL}/auth`,
     courseUrl:`${BASE_URL}/courses/create-course`,
