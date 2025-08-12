cfe99921248619089b8818244c74561b50cc46bb
Sourabhpatil <162549311+SourabhGpatilsp@users.noreply.github.com>
2025-08-12 10:46:14 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,10 @@
 const BASE_URL = 'http://localhost:8080';
 
 export const environment = {
-    production: false,
+    production: true,
     baseUrl:`${BASE_URL}`,
     apiUrl: `${BASE_URL}/auth`,
     courseUrl:`${BASE_URL}/courses/create-course`,
     courseModuleUrl:`${BASE_URL}/api/course-modules`,
     lessonUrl:`${BASE_URL}/api/lessons`
   };
-  
