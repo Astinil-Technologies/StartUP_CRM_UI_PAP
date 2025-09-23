2f124fe1a36851b090b7dd2f7afd6767b4e5393a
Sourabhpatil <patilsourabh0220@gmail.com>
2025-09-23 11:21:22 +0530

    timesheet

--- src/app/modules/Ttimesheet/component/timesheet/timesheet.component.ts
+++ src/app/modules/Ttimesheet/component/timesheet/timesheet.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-timesheet',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './timesheet.component.html',
-  styleUrl: './timesheet.component.scss'
+  styleUrls: ['./timesheet.component.scss']
 })
-export class TimesheetComponent {
+export class TimesheetComponent implements OnInit {
+  private http = inject(HttpClient);
 
+  timesheets: any[] = [];
+
+  ngOnInit() {
+    this.loadTimesheets();
+  }
+
+  loadTimesheets() {
+    this.http.get<any>('http://localhost:8888/timesheet/history')
+      .subscribe({
+        next: (res) => {
+          if (res.success && res.data) {
+            this.timesheets = res.data;
+          }
+        },
+        error: (err) => {
+          console.error('Error fetching timesheets:', err);
+        }
+      });
+  }
 }
